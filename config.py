4ff73ea46bfa695dcefa4f03d0fb65792d7815d6
Prince kumar <112043341+cutexboy@users.noreply.github.com>
2024-01-28 21:18:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/cutexboy/KID-PERSON-MUSIC",
+    "https://github.com/cutexboy/COPYRIGHT",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
